88a29bcbabeb14c0e139d1e7cb4673f4fa11498a
Alex Gabriel Yovera Loayza (HAY-LIM) <alexy@hayduk.com.pe>
2020-05-01 19:00:33 -0500

    comit inicial

---
+++ app.js
@@ -0,0 +1,36 @@
+const argv = require('./config/yargs').argv;
+const colors = require('colors');
+
+const { CrearArchivo, listarTabla } = require('./multiplicar/multiplicar');
+
+
+let comando = argv._[0];
+console.log(argv);
+
+switch (comando) {
+    case 'listar':
+        listarTabla(argv.base, argv.lmite)
+
+        break;
+
+    case 'crear':
+        CrearArchivo(argv.base, argv.limite)
+            .then(archivo => console.log(`Archivo creado: ${ archivo }`))
+            .catch(e => console.log(e))
+        break;
+    default:
+        console.log('comando no reconocido')
+        break;
+}
+
+
+
+//let parametro = argv[2];
+//let base = parametro.split('=')[1];
+//console.log('limite', argv.limite);
+//console.log(argv2);
+
+
+/*CrearArchivo(base)
+    .then(archivo => console.log(`Archivo creado: ${ archivo }`))
+    .catch(e => console.log(e))*/
